be0aac932e81917e7b2e7214f8253bb263fd457c
steve <stevenjoro098@gmail.com>
2025-03-24 18:13:18 +0300

    Admin dashboard setup

--- NewsApp/admin.py
+++ NewsApp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import GDLETNewsModel
 # Register your models here.
+
 @admin.register(GDLETNewsModel)
 class NewsAdmin(admin.ModelAdmin):
-    list_display = ['title']
+    list_display = ['title','url','socialimage']
